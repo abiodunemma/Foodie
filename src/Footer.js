16b736da7dd388731a3a4010200534d36dcf7a44
abiodun emma <abiodunemma769@gmail.com>
2024-11-26 23:02:43 +0100

    final push

--- src/Footer.js
+++ src/Footer.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import './css/Footer.css';
+import food from './assets/food.png';
+import search from './assets/search.png';
+import orders from './assets/orders.png'
+import profile from './assets/profile.png';
 
 const Footer = () => {
   return (
     <footer className="footer">
-    <div className="footer-content">
-      <p>&copy; 2024 Your Website. All Rights Reserved.</p>
+
       <div className="footer-links">
-        <a href="/privacy-policy">Privacy Policy</a>
-        <a href="/terms-of-service">Terms of Service</a>
-        <a href="/contact">Contact</a>
+        <img src={food} alt='food' className='food' />
+    
+        <img src={search} alt='search' className='search' />
+       
+        <img src={orders} alt='orders' className='orders' />
+        <img src={profile} alt='profile' className='profile' />
       </div>
-    </div>
   </footer>
   )
 }
